c976602c4261179a091a20d5a774653998917f7b
jasonhunter107 <jasonhunter107@gmail.com>
2018-03-12 22:59:55 -0400

    Hurdle Adjustment

--- Project2.srcs/sources_1/new/hurdles.vhd
+++ Project2.srcs/sources_1/new/hurdles.vhd
@@ -23,14 +23,6 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity hurdles is
     Port ( reset : in STD_LOGIC;
@@ -47,8 +39,12 @@ architecture Behavioral of hurdles is
 
 signal Object_X_pos, Object_X_pos2, Object_X_pos3 : STD_LOGIC_VECTOR(10 downto 0) := B"01001101100"; -- Decimal 620
 signal Object_Y_pos : STD_LOGIC_VECTOR(10 downto 0) := B"00011110101"; -- Decimal 245
+signal speed : integer := 1;
 signal flag : STD_LOGIC := '1';
 signal flag2, flag3 : STD_LOGIC := '0';
+signal red1, red2, red3 : STD_LOGIC_VECTOR(3 downto 0);
+signal green1, green2, green3 : STD_LOGIC_VECTOR(3 downto 0);
+signal blue1, blue2, blue3 : STD_LOGIC_VECTOR(3 downto 0);
 
 begin
 
@@ -60,14 +56,14 @@ begin
  if((((hcount - Object_X_pos) <= 15) and ((Object_X_pos - hcount) >= 15) and -- Lower rectangle
     ((vcount - 35 - Object_Y_pos) <= 2) and ((Object_Y_pos - 35 - vcount) >= 2) and (blank = '0'))
  or (((hcount - 10 - Object_X_pos) <= 5) and ((Object_X_pos - hcount) >= 5) and -- Vertical Bar
-    ((vcount - 15 - Object_Y_pos) <= 20) and (blank = '0'))) then
-      Red <= X"F";
-      Green <= X"F";
-      Blue <= X"F";
+    ((vcount - 5 - Object_Y_pos) <= 32) and (blank = '0'))) then
+      Red1 <= X"F";
+      Green1 <= X"F";
+      Blue1 <= X"F";
  else
-    Red <= X"0";
-    Green <= X"0";
-    Blue <= X"0";
+    Red1 <= X"0";
+    Green1 <= X"0";
+    Blue1 <= X"0";
   end if; 
   end if;
  
@@ -76,14 +72,14 @@ begin
   if((((hcount - Object_X_pos2) <= 15) and ((Object_X_pos2 - hcount) >= 15) and -- Lower rectangle
      ((vcount - 35 - Object_Y_pos) <= 2) and ((Object_Y_pos - 35 - vcount) >= 2) and (blank = '0'))
   or (((hcount - 10 - Object_X_pos2) <= 5) and ((Object_X_pos2 - hcount) >= 5) and -- Vertical Bar
-     ((vcount - 15 - Object_Y_pos) <= 20) and (blank = '0'))) then
-       Red <= X"F";
-       Green <= X"F";
-       Blue <= X"F";
+     ((vcount - 5 - Object_Y_pos) <= 32) and (blank = '0'))) then
+       Red2 <= X"F";
+       Green2 <= X"F";
+       Blue2 <= X"F";
   else
-     Red <= X"0";
-     Green <= X"0";
-     Blue <= X"0";
+     Red2 <= X"0";
+     Green2 <= X"0";
+     Blue2 <= X"0";
    end if; 
    end if;
    
@@ -92,14 +88,14 @@ begin
    if((((hcount - Object_X_pos3) <= 15) and ((Object_X_pos3 - hcount) >= 15) and -- Lower rectangle
       ((vcount - 35 - Object_Y_pos) <= 2) and ((Object_Y_pos - 35 - vcount) >= 2) and (blank = '0'))
    or (((hcount - 10 - Object_X_pos3) <= 5) and ((Object_X_pos3 - hcount) >= 5) and -- Vertical Bar
-      ((vcount - 15 - Object_Y_pos) <= 20) and (blank = '0'))) then
-        Red <= X"F";
-        Green <= X"F";
-        Blue <= X"F";
+      ((vcount - 5 - Object_Y_pos) <= 32) and (blank = '0'))) then
+        Red3 <= X"F";
+        Green3 <= X"F";
+        Blue3 <= X"F";
    else
-      Red <= X"0";
-      Green <= X"0";
-      Blue <= X"0";
+      Red3 <= X"0";
+      Green3 <= X"0";
+      Blue3 <= X"0";
     end if;
     end if;   
     
@@ -114,6 +110,9 @@ begin
  begin
   if(reset = '1') then
      Object_X_pos <= B"01001101100";    -- Decimal 620
+     Object_X_pos2 <= B"01001101100";    -- Decimal 620
+     Object_X_pos3<= B"01001101100";    -- Decimal 620
+     speed <= 1;
       flag <= '1';
       flag2 <= '0';
       flag3 <= '0';
@@ -122,10 +121,10 @@ begin
   ------------------------------------------------------------------------------
   if (flag = '1') then
   if(Object_X_pos  > 20) then        -- Move from R -> L
-      Object_X_pos <= Object_X_pos - 2;
+      Object_X_pos <= Object_X_pos - speed;
    elsif(Object_X_pos <= 200) then             -- Checkpoint 1       
            flag2 <= '1'; 
-  elsif(Object_X_pos = 20) then             -- Reach extreme LEFT POSITION
+  elsif(Object_X_pos <= 20) then             -- Reach extreme LEFT POSITION
       --Object_X_pos <= B"01001101100";        
        Object_X_pos <= Object_X_pos;
        flag <= '0';   
@@ -135,10 +134,10 @@ begin
 ------------------------------------------------------------------------------
   if (flag2 = '1') then
   if(Object_X_pos2  > 20) then        -- Move from R -> L
-      Object_X_pos2 <= Object_X_pos2 - 2;
+      Object_X_pos2 <= Object_X_pos2 - speed;
    elsif(Object_X_pos2 <= 200) then             -- Checkpoint 2     
                flag3 <= '1';  
-  elsif(Object_X_pos2 = 20) then             -- Reach extreme LEFT POSITION
+  elsif(Object_X_pos2 <= 20) then             -- Reach extreme LEFT POSITION
       Object_X_pos2 <= Object_X_pos2;    
       flag2 <= '0';         
     end if;
@@ -147,11 +146,15 @@ begin
   ------------------------------------------------------------------------------  
   if (flag3 = '1') then
   if(Object_X_pos3  > 20) then        -- Move from R -> L
-      Object_X_pos3 <= Object_X_pos3 - 2;
-  elsif(Object_X_pos3 = 20) then             -- Reach extreme LEFT POSITION
+      Object_X_pos3 <= Object_X_pos3 - speed;
+  elsif(Object_X_pos3 <= 20) then             -- Reach extreme LEFT POSITION
       flag <= '1';
       flag2 <= '0';
-      flag3 <='0';         
+      flag3 <='0';     
+      Object_X_pos <= B"01001101100";    -- Decimal 620
+      Object_X_pos2 <= B"01001101100";    -- Decimal 620
+      Object_X_pos3<= B"01001101100";    -- Decimal 620   
+      speed <= speed + 1; 
     end if;
   end if;
   
@@ -161,6 +164,10 @@ begin
   
   end if;
   end process;
+
+Red <= red1 or red2 or red3;
+Green <= green1 or green2 or green3;
+Blue <= blue1 or blue2 or blue3;
 
 
 end Behavioral;
